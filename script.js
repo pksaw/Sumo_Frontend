937ef44bb6f7d38a21f0f8606bfacfb7cc2c71c7
pksaw <pks25111998@gmail.com>
2018-08-25 09:53:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('#folder-1').click(function() {
+        $('#sub-folder-1').fadeToggle(10);
+        $('#right_angle-1').fadeToggle(50);
+        $('#down_angle-1').fadeToggle(50);
+
+
+    })
+})
